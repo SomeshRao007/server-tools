9b52b4d0ea67b05d4bef4fdccaf9f2e696197d71
Somesh Rao Coka <111784343+SomeshRao007@users.noreply.github.com>
2024-11-14 10:57:54 +0530

    Update __manifest__.py

--- base_exception/__manifest__.py
+++ base_exception/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Exception Rule",
-    "version": "17.0.4.2.5",
+    "version": "17.0.4.2.6",
     "development_status": "Mature",
     "category": "Generic Modules",
     "summary": """
